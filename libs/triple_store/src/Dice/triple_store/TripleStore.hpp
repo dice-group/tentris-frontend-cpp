568f8548f96cbc9927044fc182707b3289f77c82
nkaralis <nkaralis@mail.uni-paderborn.de>
2022-07-05 12:22:55 +0200

    separated implicit from explicit grouping

--- libs/triple_store/src/Dice/triple_store/TripleStore.hpp
+++ libs/triple_store/src/Dice/triple_store/TripleStore.hpp
@@ -137,14 +137,65 @@ namespace Dice::triple_store {
 		std::generator<rdf_tensor::Entry const &>
 		eval_select_aggr(const sparql2tensor::SPARQLQuery &query,
 						 std::chrono::steady_clock::time_point endtime = std::chrono::steady_clock::time_point::max()) {
+			/* implicit grouping -- all solutions belong to a single group; group by clause was not provided */
+			if (query.grouping_keys_.expressions().empty())
+				co_yield std::elements_of(eval_aggr_implicit_group(query, endtime));
+			/* explicit grouping -- group by clause was provided */
+			else
+				co_yield std::elements_of(eval_aggr_explicit_group(query, endtime));
+		}
+
+		std::generator<rdf_tensor::Entry const &>
+		eval_aggr_implicit_group(const sparql2tensor::SPARQLQuery &query,
+								 std::chrono::steady_clock::time_point endtime = std::chrono::steady_clock::time_point::max()) {
+				// the solutions of the single (implicit) group
+				auto group_solution = query.solution_.clone();
+				auto &group_solution_expressions = group_solution.expressions();
+				// the solution mapping (binding)
+				rdf_tensor::Entry solution_mapping;
+				solution_mapping.key().resize(group_solution_expressions.size());
+				auto operands = generate_operands(query.get_slice_keys());
+				// prepare the variables that need to be projected by the query library
+				std::vector<char> tracked_vars_id{};
+				tracked_vars_id.resize(query.tracked_variables_.size());
+				for (auto const &tracked_var : query.tracked_variables_) {
+					tracked_vars_id[tracked_var.second] = query.var_to_id_.at(tracked_var.first);
+				}
+				rdf_tensor::Query q{query.odg_, operands, tracked_vars_id, endtime};
+				for (auto const &entry : Dice::query::Evaluation::evaluate<htt_t, allocator_type>(q)) {
+					// update aggregate values
+					auto card = entry.value();
+					while (card > 0) {
+						for (auto &aggr : group_solution_expressions) {
+							aggr->evaluate(entry);
+						}
+						card--;
+					}
+				}
+				// generate the solution mapping
+				for (size_t i = 0; i < group_solution_expressions.size(); i++) {
+					auto sol_expr_result = group_solution_expressions[i]->result();
+					solution_mapping[i] = sol_expr_result.has_value() ? sol_expr_result.value() : rdf_tensor::NodeWrapper();
+				}
+				co_yield solution_mapping;
+		}
+
+		std::generator<rdf_tensor::Entry const &>
+		eval_aggr_explicit_group(const sparql2tensor::SPARQLQuery &query,
+								 std::chrono::steady_clock::time_point endtime = std::chrono::steady_clock::time_point::max()) {
+			// map from group key to solutions
 			std::map<rdf_tensor::Entry, sparql2tensor::expressions::ExpressionList> grouped_solutions{};
+			// the group key
 			rdf_tensor::Entry group_key;
+			// clone the expressions of the group by
 			auto grouping_keys = query.grouping_keys_.clone();
 			auto &grouping_keys_expressions = grouping_keys.expressions();
-			group_key.key().resize(grouping_keys.expressions().size());
+			group_key.key().resize(grouping_keys_expressions.size());
+			// the solution mapping (binding)
 			rdf_tensor::Entry solution_mapping;
 			solution_mapping.key().resize(query.solution_.expressions().size());
 			auto operands = generate_operands(query.get_slice_keys());
+			// prepare the variables that need to be projected by the query library
 			std::vector<char> tracked_vars_id{};
 			tracked_vars_id.resize(query.tracked_variables_.size());
 			for (auto const &tracked_var : query.tracked_variables_) {
@@ -152,13 +203,13 @@ namespace Dice::triple_store {
 			}
 			rdf_tensor::Query q{query.odg_, operands, tracked_vars_id, endtime};
 			for (auto const &entry : Dice::query::Evaluation::evaluate<htt_t, allocator_type>(q)) {
-				// prepare grouping key
+				// compute the grouping key of the generated solution
 				for (size_t i = 0; i < group_key.size(); i++) {
 					grouping_keys_expressions[i]->evaluate(entry);
 					auto expr_result = grouping_keys_expressions[i]->result();
 					group_key[i] = expr_result.has_value() ? expr_result.value() : rdf_tensor::NodeWrapper();
 				}
-				// new key, clone solution
+				// create a new group of solutions, if the computed key is new
 				if (not grouped_solutions.contains(group_key)) {
 					grouped_solutions[group_key] = query.solution_.clone();
 				}
@@ -201,7 +252,6 @@ namespace Dice::triple_store {
 			}
 		}
 
-	private:
 		/**
 		 * @brief Generates the tensor operands of a query
 		 * @param slice_keys The slice keys corresponding to the query being evaluated
