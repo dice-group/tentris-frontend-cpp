c90fa59092aa89187a41fa362882789099dcf994
Liss Heidrich <Clueliss@users.noreply.github.com>
2022-09-03 20:52:28 +0200

    added body with mutation count to update response

--- libs/endpoint/src/dice/endpoint/SparqlUpdateEndpoint.cpp
+++ libs/endpoint/src/dice/endpoint/SparqlUpdateEndpoint.cpp
@@ -2,6 +2,9 @@
 
 #include "dice/endpoint/ParseSPARQLUpdateParam.hpp"
 
+#include <rapidjson/stringbuffer.h>
+
+#include <rapidjson/writer.h>
 #include <spdlog/spdlog.h>
 
 namespace dice::endpoint {
@@ -26,10 +29,26 @@ namespace dice::endpoint {
 						spdlog::debug("removing triple ({}, {}, {})", entry[0], entry[1], entry[2]);
 					}
 
+					size_t const size_before = triplestore_.size();
 					triplestore_.remove(update_query.entries_for_removal);
+					size_t const mutation_count = size_before - triplestore_.size();
 
-					req->create_response(status_ok()).done();
-					spdlog::info("HTTP response {}", status_ok());
+					rapidjson::StringBuffer buf;
+					{
+						rapidjson::Writer<rapidjson::StringBuffer> jw{buf};
+
+						jw.StartObject();
+						jw.Key("mutation_count");
+						jw.Uint64(mutation_count);
+						jw.EndObject();
+					}
+
+					req->create_response(status_ok())
+							.append_header(http_field::content_type, "application/json")
+							.set_body(std::string{buf.GetString(), buf.GetSize()})
+							.done();
+
+					spdlog::info("HTTP response {}, mutation_count: {}", status_ok(), mutation_count);
 				} catch (std::runtime_error const &e) {
 					static constexpr auto message = "Invalid Content-Type";
 
