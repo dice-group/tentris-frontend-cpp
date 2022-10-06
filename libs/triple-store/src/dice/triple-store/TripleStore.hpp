507206c5e026decacc6e74b8aafdfcfda4e77321
Liss Heidrich <Clueliss@users.noreply.github.com>
2022-10-06 17:22:58 +0200

    increse perf of bulk removal

--- libs/triple-store/src/dice/triple-store/TripleStore.hpp
+++ libs/triple-store/src/dice/triple-store/TripleStore.hpp
@@ -63,17 +63,14 @@ namespace dice::triple_store {
 		}
 
 		void remove_parse(std::string_view query, uint32_t bulk_size = 1'000'000) {
-			std::vector<hypertrie::internal::raw::SingleEntry<3, htt_t>> entries;
-
-			AddTripleCallback_function add_entry_callback = [&entries](rdf4cpp::rdf::Node subj, rdf4cpp::rdf::Node pred, rdf4cpp::rdf::Node obj) noexcept {
-				entries.push_back(hypertrie::internal::raw::SingleEntry<3, htt_t>{{subj, pred, obj}});
-			};
-			serd_load_delete_data_triples_from_string(query, add_entry_callback);
-
 			HypertrieBulkRemover bulk_remover{hypertrie_, bulk_size};
-			for (auto const &e : entries) {
-				bulk_remover.add(e);
-			}
+
+			AddTripleCallback_function add_entry_callback = [&](rdf4cpp::rdf::Node subj, rdf4cpp::rdf::Node pred, rdf4cpp::rdf::Node obj) noexcept {
+				bulk_remover.add(hypertrie::internal::raw::SingleEntry<3, htt_t>{{subj, pred, obj}});
+				//hypertrie_.set(Key{subj, pred, obj}, false);
+			};
+
+			serd_load_delete_data_triples_from_string(query, add_entry_callback);
 		}
 
 		void add_statement(const rdf4cpp::rdf::Statement &statement) {
