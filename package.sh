69da90f2ae8bdc2a798c3f795af7bc0f60ab431c
Liss Heidrich <Clueliss@users.noreply.github.com>
2023-08-14 13:58:07 +0200

    allow extra args for package

--- package.sh
+++ package.sh
@@ -6,6 +6,8 @@ CONAN_USER="$1"
 CONAN_PW="$2"
 IMAGE_NAME="tentris-server-rs"
 
+shift 2
+
 if podman --version > /dev/null; then
     echo "Using podman"
     BUILDER="podman"
@@ -17,7 +19,7 @@ else
     exit 1
 fi
 
-${BUILDER} build . --tag "${IMAGE_NAME}" --build-arg CONAN_USER=${CONAN_USER} --build-arg CONAN_PW=${CONAN_PW} --ssh=default
+${BUILDER} build . --tag "${IMAGE_NAME}" --build-arg CONAN_USER=${CONAN_USER} --build-arg CONAN_PW=${CONAN_PW} --ssh=default "$@"
 CONTAINER_ID=$(${BUILDER} container create "${IMAGE_NAME}")
 ${BUILDER} cp "${CONTAINER_ID}:/tentris-server-rs" .
 ${BUILDER} container rm "${CONTAINER_ID}"
