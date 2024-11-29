5679adc4b634ae571b3c68ee739068d79363b111
Liss Heidrich <31625940+liss-h@users.noreply.github.com>
2024-11-29 09:53:19 +0100

    port loader

--- libs/triple-store/src/dice/triple-store/TripleStore.cpp
+++ libs/triple-store/src/dice/triple-store/TripleStore.cpp
@@ -138,7 +138,7 @@ namespace dice::triple_store {
 		if (query.triple_patterns_.size() == 1) {// O(1)
 			auto slice_key = query.get_slice_keys()[0];
 			if (slice_key.get_fixed_depth() == 3)
-				return (size_t) std::get<bool>(get_hypertrie()[slice_key]);
+				return static_cast<size_t>(std::get<bool>(get_hypertrie()[slice_key]));
 			else
 				return std::get<const_BoolHypertrie>(get_hypertrie()[slice_key]).size();
 		} else {
